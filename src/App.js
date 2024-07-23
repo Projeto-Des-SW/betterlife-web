b750ad13a361922e60b23d221849b3542bc542e5
Rian Wilker Santos Melo <78122002+RWilker87@users.noreply.github.com>
2024-07-22 22:06:19 -0300

    Adicionado roda de redefinir senha

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './Pages/Login/Login';
 import Register from './Pages/Register/Register';
 import RecuperarSenha from './Pages/RecuperarSenha/RecuperarSenha';
 import TelaPrincipal from './Pages/TelaPrincipal/TelaPrincipal';
+import RedefinirSenha from './Pages/RedefinirSenha/RedefinirSenha';
 //Context
 import { UserProvider } from './Contexts/UserContext';
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path='/telaPrincipal' element={<TelaPrincipal/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/recuperarSenha' element={<RecuperarSenha />} />
+        <Route path='/redefinirSenha' element={<RedefinirSenha/>} />
       </Routes>
     </div>
     </BrowserRouter>
