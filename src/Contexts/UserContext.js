6332567d5d05b78a3cfbefde39ba4586e67fb90b
Daniel Duarte <danielduartefilho.df@gmail.com>
2024-07-31 21:41:27 -0300

    removido imports do UserContext

--- src/Contexts/UserContext.js
+++ src/Contexts/UserContext.js
@@ -1,9 +1,15 @@
-import React, { createContext, useState } from 'react';
+import React, { useState, createContext } from 'react';
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({
+    email: '',
+    nome: '',
+    documento: '',
+    telefone: '',
+    tipousuario: ''
+  });
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
