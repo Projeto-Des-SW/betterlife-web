0e4706c7924756c1716020fc89c90b81ca5c4237
Daniel Duarte <danielduartefilho.df@gmail.com>
2024-09-11 20:34:28 -0300

    adicionado botão de voltar na tela de registrar foruns

--- src/Pages/RegisterPostForum/RegisterPostForum.jsx
+++ src/Pages/RegisterPostForum/RegisterPostForum.jsx
@@ -9,7 +9,11 @@ const RegisterPostForum = () => {
     const navigate = useNavigate();
     const [usuarioIdPergunta, setUsuarioIdPergunta] = useState('');
     const [pergunta, setPergunta] = useState('');
-    const [categoriaForumId, setCategoriaForumId] = useState('1'); // Valor inicial pode ser o ID de uma categoria padrão
+    const [categoriaForumId, setCategoriaForumId] = useState('1');
+
+    const handleBack = () => {
+        navigate('/telaPrincipal');
+      };
 
     useEffect(() => {
         const userId = dadosUserLogadoService.getUserInfo().id;
@@ -18,67 +22,71 @@ const RegisterPostForum = () => {
 
     const submeter = async (e) => {
         e.preventDefault();
-        if(!pergunta || !categoriaForumId){
+        if (!pergunta || !categoriaForumId) {
             alert('Todos os campos do post são obrigatórios.');
             return;
         }
-        try{            
-            const post = { 
+        try {
+            const post = {
                 usuarioidpergunta: usuarioIdPergunta,
                 pergunta: pergunta,
-                categoriaforumid: categoriaForumId };
+                categoriaforumid: categoriaForumId
+            };
             const response = await forumService.registerPost(JSON.stringify(post));
             console.log(response.error)
             if (response.error === false) {
                 alert('Post enviado');
                 setTimeout(() => {
-                    navigate('/telaPrincipal'); //Mudar a rota para a rota do post que acabou de ser enviado
-                }, 2000); 
+                    navigate('/telaPrincipal');
+                }, 2000);
                 setUsuarioIdPergunta('');
                 setPergunta('');
-                setCategoriaForumId('1'); 
+                setCategoriaForumId('1');
             }
 
         } catch (error) {
             alert(error.message || 'Erro ao realizar post no forum');
-        }    
+        }
     };
 
-  return (
-    <>
-    <Header />
-    <div className={Styles.ConteudoContainer}>
-            <h1>Criar Post</h1>
-            <form onSubmit={submeter}>                
-                <div className={Styles.formGroup}>
-                    <label htmlFor="pergunta">Pergunta:</label>
-                    <textarea 
-                        id="pergunta" 
-                        rows="5" 
-                        value={pergunta} 
-                        onChange={(e) => setPergunta(e.target.value)} 
-                        required 
-                    />
+    return (
+        <>
+            <Header />
+            <div className={Styles.ConteudoContainer}>
+                <h1>Criar Post</h1>
+                <form onSubmit={submeter}>
+                    <div className={Styles.formGroup}>
+                        <label htmlFor="pergunta">Pergunta:</label>
+                        <textarea
+                            id="pergunta"
+                            rows="5"
+                            value={pergunta}
+                            onChange={(e) => setPergunta(e.target.value)}
+                            required
+                        />
+                    </div>
+
+                    <div className={Styles.formGroup}>
+                        <label htmlFor="categoriaForumId">Categoria:</label>
+                        <select
+                            id="categoriaForumId"
+                            value={categoriaForumId}
+                            onChange={(e) => setCategoriaForumId(e.target.value)}
+                            required
+                        >
+                            <option value="1">Discussão</option>
+                            <option value="2">Dúvida</option>
+                            <option value="3">Compartilhamento</option>
+                        </select>
+                    </div>
+                    <button type="submit" className={Styles.PostButton}>Publicar</button>
+                </form>
+                <div className={Styles.buttonContainer}>
+                    <button type="button" className={Styles.VoltarButton} variant="contained" color="default" onClick={handleBack}>Voltar</button>
                 </div>
-                
-                <div className={Styles.formGroup}>
-                    <label htmlFor="categoriaForumId">Categoria:</label>
-                    <select 
-                        id="categoriaForumId" 
-                        value={categoriaForumId} 
-                        onChange={(e) => setCategoriaForumId(e.target.value)} 
-                        required
-                    >
-                        <option value="1">Discussão</option>
-                        <option value="2">Dúvida</option>
-                        <option value="3">Compartilhamento</option>
-                    </select>
-                </div>
-                <button type="submit" className={Styles.PostButton}>Publicar</button>
-            </form>
-        </div>
-    </>
-  )
+            </div>
+        </>
+    )
 }
 
 export default RegisterPostForum
