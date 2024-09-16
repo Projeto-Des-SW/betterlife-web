5a9bb54ca9dca191bca2fcdbdc1d7aacffa6189a
David Lucas Flayban <davidflayban2@hotmail.com>
2024-09-16 20:18:56 -0300

    Implementado acesso a telas de Departamento
    Implementado o acesso atraves de botões para as rotas permitidas para um departamento

--- src/Pages/TelaPrincipal/Components/Departamento/DepartamentoComponent.jsx
+++ src/Pages/TelaPrincipal/Components/Departamento/DepartamentoComponent.jsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import Styles from './DepartamentoComponent.module.css';
+import { useNavigate } from 'react-router-dom';
+import {
+  Paper
+} from '@material-ui/core';
 
-function DepartmentoComponent() {
-  return <div>Conteúdo exclusivo para Departamento</div>;
+function DepartamentoComponent() {
+  const navigate = useNavigate();
+
+  const TaxonomiaPage = () => {
+    navigate('/taxonomia');
+  };
+
+  const AnimalPage = () => {
+    navigate('/registerAnimal');
+  };
+
+  const AnimaisPage = () => {
+    navigate('/animais')
+  };
+
+  const CategoriaForumPage = () => {
+    navigate('/categoriaForum')
+  };
+
+  const Forum = () => {
+    navigate('/RegisterPostForum')
+  };
+
+  const meusPosts = () => {
+    navigate('/meusPosts')
+  };
+
+  const minhasComunidade = () => {
+    navigate('/minhasComunidades')
+  };
+
+  const Comunidades = () => {
+    navigate('/comunidades')
+  };
+  
+
+  return (
+    <>
+      <Paper className={Styles.paper}>
+        <div className={Styles.buttonContainer}>
+          <button type="button" className={Styles.DepartamentoButton} onClick={() => TaxonomiaPage()}>Taxonomia</button>
+          <button type="button" className={Styles.DepartamentoButton} onClick={() => AnimaisPage()}>Animais</button>
+          <button type="button" className={Styles.DepartamentoButton} onClick={() => AnimalPage()}>Cadastrar Animal</button>
+          <button type="button" className={Styles.DepartamentoButton} onClick={() => Forum()}>Criar Post</button>
+          <button type="button" className={Styles.DepartamentoButton} onClick={() => CategoriaForumPage()}>Cadastrar Categoria de fórum</button>
+          <button type="button" className={Styles.DepartamentoButton} onClick={() => meusPosts()}>Meus Posts</button>
+          <button type="button" className={Styles.DepartamentoButton} onClick={() => minhasComunidade()}>Minhas Comunidades</button>
+          <button type="button" className={Styles.DepartamentoButton} onClick={() => Comunidades()}>Comunidades</button>
+        </div>
+      </Paper>
+    </>
+  )
 }
 
-export default DepartmentoComponent;
+export default DepartamentoComponent;
