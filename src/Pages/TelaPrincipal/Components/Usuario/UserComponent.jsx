2e372ea50e67f0a5b5e45cd2b32540d385be75d2
Rian Wilker Santos Melo <78122002+RWilker87@users.noreply.github.com>
2024-09-18 00:02:20 -0300

    Arrumando a tela para tipo de usuário

--- src/Pages/TelaPrincipal/Components/Usuario/UserComponent.jsx
+++ src/Pages/TelaPrincipal/Components/Usuario/UserComponent.jsx
@@ -1,36 +1,28 @@
 import React from 'react';
 import Styles from './UserComponent.module.css';
 import { useNavigate } from 'react-router-dom';
-import {
-  Paper
-} from '@material-ui/core';
+import { Paper } from '@material-ui/core';
+
 function UserComponent() {
   const navigate = useNavigate();
 
-  const AnimaisPage = () => {
-    navigate('/animais')
-  };
-
-  const Forum = () => {
-    navigate('/RegisterPostForum')
-  };
-
-  const meusPosts = () => {
-    navigate('/meusPosts')
-  };
-
-  const Comunidades = () => {
-    navigate('/comunidades')
-  };
+  const AnimaisPage = () => navigate('/animais');
+  const Forum = () => navigate('/RegisterPostForum');
+  const meusPosts = () => navigate('/meusPosts');
+  const Comunidades = () => navigate('/comunidades');
 
   return (
     <>
       <Paper className={Styles.paper}>
-        <div className={Styles.buttonContainer}>          
-          <button type="button" className={Styles.UserButton} onClick={() => AnimaisPage()}>Animais</button>
-          <button type="button" className={Styles.UserButton} onClick={() => Forum()}>Criar Post</button>          
-          <button type="button" className={Styles.UserButton} onClick={() => meusPosts()}>Meus Posts</button>          
-          <button type="button" className={Styles.UserButton} onClick={() => Comunidades()}>Comunidades</button>
+        <div className={Styles.buttonContainer}>
+          <div className={Styles.buttonGroup}>
+            <button type="button" className={Styles.UserButton} onClick={AnimaisPage}>Animais</button>
+            <button type="button" className={Styles.UserButton} onClick={Forum}>Criar Post</button>
+          </div>
+          <div className={Styles.buttonGroup}>
+            <button type="button" className={Styles.UserButton} onClick={meusPosts}>Meus Posts</button>
+            <button type="button" className={Styles.UserButton} onClick={Comunidades}>Comunidades</button>
+          </div>
         </div>
       </Paper>
     </>
